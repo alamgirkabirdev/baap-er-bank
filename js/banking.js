bf6f7625d8383edd748a9ea5eb1d1978f5630493
Md Mazharul Islam <mazharulislam.dev@gmail.com>
2022-09-24 16:03:58 +0600

    fix this problem

--- js/banking.js
+++ js/banking.js
@@ -1,6 +1,6 @@
 // handle deposit button event
-document.getElementById('deposit-button').addEventListener('click', function(){
-    const depositInput  = document.getElementById('deposit-input');
-    const depositAmount= depositInput.value;
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-amount');
+    const depositAmount = depositInput.value;
     console.log(depositAmount);
 })
